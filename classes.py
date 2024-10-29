960b688770945917e1880125674f99a3e2011035
bgo <bgo@quicktext.im>
2024-10-29 12:05:45 +0100

    applyed pre-commit changes to these files

--- classes.py
+++ classes.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel
 
+
 class IrisInput(BaseModel):
     SepalLengthCm: float
     SepalWidthCm: float
--- main.py
+++ main.py
@@ -1,24 +1,27 @@
+import torch
 from fastapi import FastAPI
-# from models import Model
+
 from classes import IrisInput
 from predict import Predict
-import torch
+
+# from models import Model
 
 app = FastAPI()
 
 loaded_model = None
 
-# loaded_model = Model() 
+# loaded_model = Model()
 # loaded_model.load_state_dict(torch.load('./saved_models/iris_model.pth'))
 
-loaded_model = torch.jit.load('./saved_models/traced_model.pth')
-loaded_model.eval() 
+loaded_model = torch.jit.load("./saved_models/traced_model.pth")
+loaded_model.eval()
+
 
 @app.post("/predict")
 async def predict(input_data: IrisInput):
     if loaded_model is None:
         return {"error": "Model is not loaded."}
-    
+
     flower = Predict(loaded_model, input_data)
-    
+
     return {"predicted_class": flower}
